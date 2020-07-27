6f4376d00da36f977cf437f7dda8feea99acedda
maglweb <823358533@qq.com>
2020-07-27 20:22:04 +0800

    Test

--- apitest/views.py
+++ apitest/views.py
@@ -9,4 +9,21 @@ from django.contrib.auth import authenticate,login
  #   return HttpResponse('hello test')
 
 def login(request):
+    if request.POST:
+        username = password = ' '
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username,password=password)
+        if user is not None and user.is_active:
+            auth.login(request,user)
+            request.session['user'] = username
+            response = HttpResponseRedirect('/home/')
+            return response
+        else:
+            return render(request,'login.html',{'error':'username or password error'})
+
     return render(request,'login.html')
+   # return render(request,'login.html')
+
+def home(request):
+    return render(request,'home.html')
